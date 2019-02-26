6dea59a74ea235be2f7358b94345349b6bc8ba20
geewizz246 <geewizz246@users.noreply.github.com>
2019-02-26 11:23:01 -0500

    Added tests for backend services

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
@@ -1,9 +1,27 @@
 package ca.mcgill.ecse321.eventregistration;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.mcgill.ecse321.eventregistration.dao.EventRepository;
+import ca.mcgill.ecse321.eventregistration.dao.PersonRepository;
+import ca.mcgill.ecse321.eventregistration.dao.RegistrationRepository;
+import ca.mcgill.ecse321.eventregistration.service.EventRegistrationService;
+import ca.mcgill.ecse321.eventregistration.controller.EventRegistrationRestController;
+import ca.mcgill.ecse321.eventregistration.model.Person;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +29,54 @@ public class EventRegistrationApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Mock
+	private PersonRepository personDao;
+	@Mock
+	private EventRepository eventDao;
+	@Mock
+	private RegistrationRepository registrationDao;
+	
+	@InjectMocks
+	private EventRegistrationService service;
+	
+	@Mock
+	private EventRegistrationService serviceMock;
+	
+	@InjectMocks
+	private EventRegistrationRestController controller;
+	
+	private Person person;
+	private static final String PERSON_KEY = "TestPerson";
+	private static final String NONEXISTING_KEY = "NotAPerson";
+	
+	@Before
+	public void setMockOutput() {
+		when(personDao.findPersonByName(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
+			if(invocation.getArgument(0).equals(PERSON_KEY)) {
+				Person person = new Person();
+				person.setName(PERSON_KEY);
+				return person;
+			} else {
+				return null;
+			}
+		});
+	}
+	
+	@Before
+	public void setupMock() {
+		person = mock(Person.class);
+	}
+
+	@Test
+	public void testMockPersonCreation() {
+		assertNotNull(person);
+	}
+
+	@Test
+	public void testParticipantQueryFound() {
+	  assertEquals(PERSON_KEY, service.getPerson(PERSON_KEY).getName());
 	}
 
 }
